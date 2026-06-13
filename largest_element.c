a640fdfd1055a3f3f355c7fa9ea3d59cb97e5116
Nafisa Hossain Esha (sia) <nafisasia00@gmail.com>
2026-06-13 18:07:09 +0600

    Add program to find largest element in array
    Implement a program to find the largest element in an array of integers.

---
+++ largest_element.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a[5], max;
+
+    for(int i=0;i<5;i++){
+        scanf("%d",&a[i]);
+    }
+
+    max = a[0];
+
+    for(int i=1;i<5;i++){
+        if(a[i] > max) max = a[i];
+    }
+
+    printf("Max = %d\n", max);
+    return 0;
+}
