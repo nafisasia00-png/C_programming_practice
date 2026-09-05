fe7d4334bffb2546b3b3c49020a6b3889a99d899
Nafisa Hossain Esha (sia) <nafisasia00@gmail.com>
2026-09-06 00:08:18 +0600

    Add multiplication table program in C

---
+++ multiplication_table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("\nMultiplication Table of %d\n", num);
+    printf("--------------------------\n");
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
